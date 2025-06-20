801766402574325b51c757e496ef9ec3585707b9
Sleeptilldawn <rjoi32741@gmail.com>
2025-06-20 15:56:28 -0600

    Add files via upload

---
+++ S05_R01_Modulo2/MeridianPrime/src/test/java/Sesion05/Reto01/MeridianPrime/MeridianPrimeControlTests.java
@@ -0,0 +1,13 @@
+package Sesion05.Reto01.MeridianPrime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/* @SpringBootTest
+class MeridianPrimeControlTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+} */
---
+++ S05_R02_Modulo2/MonitoreoUCI/src/test/java/Sesion05/Reto02/MonitoreoUCI/MonitoreoUciApplicationTests.java
@@ -0,0 +1,13 @@
+package Sesion05.Reto02.MonitoreoUCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoUciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
